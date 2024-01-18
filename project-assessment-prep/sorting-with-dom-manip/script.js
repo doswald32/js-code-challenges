fcd1087ca572af1301c51aab8ab2f4f009146f53
doswald32 <dan.oswald32@gmail.com>
2024-01-18 14:41:00 -0700

    completed lab

--- project-assessment-prep/sorting-with-dom-manip/script.js
+++ project-assessment-prep/sorting-with-dom-manip/script.js
@@ -12,4 +12,31 @@ const peopleArr = [
     { id: 5, name: 'Adette', age: 9 },
     { id: 6, name: 'Gio', age: 15 }
   ];
+
+  function sortedByAge(array) {
+    array.sort(function(a, b) {
+      return a.age - b.age;
+    });
+    return array;
+  };
+
+  document.addEventListener('DOMContentLoaded', function() {
+    renderPeople(peopleArr);
+  });
   
+  function renderPeople(array) {
+    let ul = document.getElementById('list');
+    ul.innerHTML = '';
+    array.forEach(function(person) {
+        let li = document.createElement('li');
+        li.textContent = `${person.name} is ${person.age} years old.`;
+        ul.appendChild(li);
+    });
+  };
+
+  let sortBtn = document.getElementById('sortButton');
+  sortBtn.addEventListener('click', function() {
+    let newArray = sortedByAge(peopleArr);
+    console.log(newArray);
+    renderPeople(newArray);
+  });
